340cb7a8d2ebe43ac5b4979c68cf1b08b2e10727
mimilus <mimilusubuntu@gmail.com>
2014-04-11 09:25:29 +0200

    first commit

---
+++ init.lua
@@ -0,0 +1,7 @@
+minetest.register_node("desert-cobble:desert_cobble", {
+    description = "Desert Cobble",
+    tiles = {"default_desert_cobble.png"},
+    is_ground_content = true,
+    groups = {cracky=3, stone=2},
+    sounds = default.node_sound_stone_defaults(),
+})
