d663d14f8348345086aa3249b3ac31a6e54ba801
mimilus <mimilusubuntu@gmail.com>
2014-04-11 10:20:00 +0200

    Added depend.txt :[ forget it

--- init.lua
+++ init.lua
@@ -1,7 +1,9 @@
-minetest.register_node("desert-cobble:desert_cobble", {
-    description = "Desert Cobble",
-    tiles = {"desert_cobble.png"},
-    is_ground_content = true,
-    groups = {cracky=3, stone=2},
-    sounds = default.node_sound_stone_defaults(),
+minetest.register_node("desertcobble:desert_cobble", {
+	description = "Desert Cobble",
+	drawtype = "normal",
+	tiles = {"desert_cobble.png"},
+	paramtype = light,
+	drop = "desertcobble:desert_cobble",
+	groups = {cracky=3},
+	sounds = default.node_sound_stone_defaults(),
 })
